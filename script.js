1dce5ad24e2e738125d816824ab5aa5e32d9a643
Pallavi-02-khandate <pkhandate2002@gmail.com>
2026-01-30 12:12:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+
+const inputs = document.querySelectorAll(".code");
+
+// Focus first box on load
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+
+  // Move forward when typing
+  input.addEventListener("input", () => {
+    if (input.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  // Move backward on backspace
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      }
+    }
+  });
+
+});
+
